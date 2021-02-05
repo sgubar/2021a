802776744a858673a9445f15bc929b6cbcb57fc4
sgubar <s.gubar@kpi.ua>
2021-02-05 10:54:48 +0200

    Add files via upload

---
+++ Slava/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  d1
+//
+//  Created by Slava Gubar on 04.02.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
